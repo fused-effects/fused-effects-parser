2ec3b578c4cbc030efe00cb3aa5ef5a7aa21e38c
Rob Rix <rob.rix@me.com>
2019-12-17 03:06:37 -0500

    Define a Parser carrier.

--- src/Control/Carrier/Parser/Church.hs
+++ src/Control/Carrier/Parser/Church.hs
@@ -1,2 +1,20 @@
+{-# LANGUAGE RankNTypes #-}
 module Control.Carrier.Parser.Church
-() where
+( -- * Parser carrier
+  ParserC(..)
+) where
+
+import Data.Text.Prettyprint.Doc
+import Data.Text.Prettyprint.Doc.Render.Terminal
+import Source.Span
+
+newtype ParserC m a = ParserC
+  { runParserC
+    :: forall r
+    .  (Pos -> String -> a -> m r)           -- success
+    -> (Pos -> Maybe (Doc AnsiStyle) -> m r) -- empty
+    -> (Pos -> Maybe (Doc AnsiStyle) -> m r) -- cut
+    -> Pos
+    -> String
+    -> m r
+  }
