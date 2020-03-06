0da7dd10621a7cddf1081bd1c1fb8dc9a8bba7af
Rob Rix <rob.rix@me.com>
2020-03-05 20:24:53 -0500

    Rename some error parameters.

--- src/Control/Carrier/Parser/Church.hs
+++ src/Control/Carrier/Parser/Church.hs
@@ -171,7 +171,7 @@ instance (Algebra sig m, Effect sig) => Algebra (Parser :+: Cut :+: NonDet :+: s
         >>= k
 
       Label m s k  ->
-        ParserC (\ leaf nil fail -> runParserC m leaf (\ i r -> nil i (r <|> Just (pretty s))) (\ i r -> fail i (r <|> Just (pretty s))))
+        ParserC (\ leaf nil fail -> runParserC m leaf (\ i e -> nil i (e <|> Just (pretty s))) (\ i e -> fail i (e <|> Just (pretty s))))
         >>= k
 
       Unexpected s -> ParserC $ \ _ nil _ input -> nil input (Just (pretty s))
