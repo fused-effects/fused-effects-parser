64a3017e74fb1f716f6a1bb2ee146f0a6aea7f18
Rob Rix <rob.rix@me.com>
2020-03-07 00:41:07 -0500

    Derive a Pretty instance for Path.

--- src/Control/Effect/Parser/Path.hs
+++ src/Control/Effect/Parser/Path.hs
@@ -1,12 +1,14 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Control.Effect.Parser.Path
 ( Path(..)
 , path
 ) where
 
 import Control.Effect.Reader
+import Data.Text.Prettyprint.Doc
 
 newtype Path = Path { getPath :: FilePath }
-  deriving (Eq, Ord, Show)
+  deriving (Eq, Ord, Pretty, Show)
 
 path :: Has (Reader Path) sig m => m FilePath
 path = asks getPath
