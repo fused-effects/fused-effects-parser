9e23c338f8ba268378bd9ee84fbad32089671386
Rob Rix <rob.rix@me.com>
2020-03-07 00:50:13 -0500

    Define a lens constructor.

--- src/Control/Effect/Parser/Lens.hs
+++ src/Control/Effect/Parser/Lens.hs
@@ -1,6 +1,11 @@
 {-# LANGUAGE RankNTypes #-}
 module Control.Effect.Parser.Lens
 ( Lens'
+, lens
 ) where
 
 type Lens' s a = forall f . Functor f => (a -> f a) -> (s -> f s)
+
+lens :: (s -> a) -> (s -> a -> s) -> Lens' s a
+lens get put afa s = fmap (put s) (afa (get s))
+{-# INLINE lens #-}
