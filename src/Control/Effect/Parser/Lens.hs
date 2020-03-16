1273fd3910d8d87176bd2b7c8b6853da4c4de8f2
Rob Rix <rob.rix@me.com>
2020-03-16 14:39:03 -0400

    Define a setter operation.

--- src/Control/Effect/Parser/Lens.hs
+++ src/Control/Effect/Parser/Lens.hs
@@ -2,10 +2,20 @@
 module Control.Effect.Parser.Lens
 ( Lens'
 , lens
+, (.~)
 ) where
+
+import Data.Coerce (coerce)
+import Data.Functor.Identity
 
 type Lens' s a = forall f . Functor f => (a -> f a) -> (s -> f s)
 
 lens :: (s -> a) -> (s -> a -> s) -> Lens' s a
 lens get put afa s = fmap (put s) (afa (get s))
 {-# INLINE lens #-}
+
+(.~) :: Lens' s a -> a -> s -> s
+l .~ b = coerce . l (const (Identity b))
+{-# INLINE (.~) #-}
+
+infixr 4 .~
