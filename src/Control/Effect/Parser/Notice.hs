aec1c2f157c4f962eac792f67363b050dbddbabc
Rob Rix <rob.rix@me.com>
2020-09-22 07:24:41 -0400

    Indicate the newline.

--- src/Control/Effect/Parser/Notice.hs
+++ src/Control/Effect/Parser/Notice.hs
@@ -57,7 +57,7 @@ prettyNotice (Notice level (Excerpt path line span) reason context) = vsep
     , reason
     ]))
   : blue (pretty (succ (Span.line (Span.start span)))) <+> align (vcat
-    [ blue (pretty '|') <+> if "\n" `isSuffixOf` line then pretty (init line) else pretty line <> blue (pretty "<end of input>")
+    [ blue (pretty '|') <+> if "\n" `isSuffixOf` line then pretty (init line) <> blue (pretty "\\n") else pretty line <> blue (pretty "<end of input>")
     , blue (pretty '|') <+> padding span <> caret span
     ])
   : context)
