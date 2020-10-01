38b652307f226d0184ed43c1193038902e44175b
Rob Rix <rob.rix@me.com>
2020-10-01 17:55:26 -0400

    Write a grammar that we can fuss with.

--- src/Control/Effect/Parser/GHCI.hs
+++ src/Control/Effect/Parser/GHCI.hs
@@ -1,10 +1,72 @@
 module Control.Effect.Parser.GHCI
 ( parse
+, sig
 ) where
 
+import Control.Applicative
 import Control.Effect.Parser.Notice
-import Control.Effect.Parser.Span
+import Control.Effect.Parser.Span hiding (line)
 import Control.Carrier.Parser.Church
+import Text.Parser.Char
+import Text.Parser.Combinators
+import Text.Parser.Token
+import Prettyprinter (line)
+import Prettyprinter.Render.Terminal
+import Debug.Trace
 
-parse :: ParserC (Either Notice) a -> String -> Either Notice a
-parse p s = runParserWithString (Pos 0 0) s p
+parse :: Show a => ParserC (Either Notice) a -> String -> IO ()
+parse p s = do
+  let v = runParserWithString (Pos 0 0) s p
+  either (putDoc . (<> line) . prettyNotice) print v
+
+sig :: (TokenParsing m, Monad m) => m String
+sig = sig' _Type _Type
+
+sig' :: (TokenParsing m, Monad m) => m String -> m String -> m String
+sig' t e
+  =   q t e
+  <|> f t e
+  <|> (++) <$> type' t <*> expr
+
+q t e = do { v <-      braces (tvar <* colon <* type' t)  <* arrow ; traceM v ; sig' (var v <|> t) e } <?> "forall"
+f t e = do { v <- try (symbolic '(' *> evar) <* colon <* type' t <* symbolic ')' <* arrow ; traceM v ; sig' t (var v <|> e) } <?> "function type"
+
+type' :: TokenParsing m => m String -> m String
+type' v = v <|> _Type <|> parens (type' v) <?> "type"
+
+arrow :: TokenParsing m => m String
+arrow = symbol "->"
+
+expr :: TokenParsing m => m String
+expr = braces (pure "") <?> "expr"
+
+evar :: TokenParsing m => m String
+evar = token ((:) <$> lower <*> many alphaNum) <?> "evar"
+
+tvar :: TokenParsing m => m String
+tvar = token ((:) <$> upper <*> many alphaNum) <?> "tvar"
+
+_Type :: TokenParsing m => m String
+_Type = token (symbol "Type") <?> "Type"
+
+var :: TokenParsing m => String -> m String
+var v = token (symbol v) <?> v
+
+{-
+
+m
+  eo
+  (\e ->
+    n
+      (\a e' -> eo a (e <> e'))
+      (\e' -> ee (e <> e'))
+      co
+      ce
+      d
+      bs)
+  co
+  ce
+  d
+  bs
+
+-}
