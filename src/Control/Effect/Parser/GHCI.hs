0685978787e9ee42652d73351cf36cd25cde44b2
Rob Rix <rob.rix@me.com>
2020-10-01 16:45:24 -0400

    Define a parse helper.

--- src/Control/Effect/Parser/GHCI.hs
+++ src/Control/Effect/Parser/GHCI.hs
@@ -1,2 +1,10 @@
 module Control.Effect.Parser.GHCI
-() where
+( parse
+) where
+
+import Control.Effect.Parser.Notice
+import Control.Effect.Parser.Span
+import Control.Carrier.Parser.Church
+
+parse :: ParserC (Either Notice) a -> String -> Either Notice a
+parse p s = runParserWithString (Pos 0 0) s p
